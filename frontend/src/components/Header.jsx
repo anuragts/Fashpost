493d5b1fcf56a324724a4aceeec087730b72dc73
Anurag <anuragsharma011011@gmail.com>
2022-07-09 03:18:00 +0530

    Refactor

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaSignOutAlt, FaSignInAlt, FaUser } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, reset } from "../features/auth/authSlice";
 
