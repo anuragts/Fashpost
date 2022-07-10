183a57675a1dd43ea50c100aaae8a4ab4dc43e71
Anurag <anuragsharma011011@gmail.com>
2022-07-10 15:40:44 +0530

    Model reconfig

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -11,6 +11,22 @@ const goalSchema = mongoose.Schema(
       type: String,
       requires: [true, "Please add text value"],
     },
+    location:{
+      type: String,
+      required: [true, "Please add location value"],
+    },
+    imageurl: {
+      type: String,
+      required: [true, "Please add image url value"],
+    },
+    website:{
+      type: String,
+      required: [true, "Please add website value"],
+    },
+    date: {
+      type: Date,
+      required: [true, "Please add date value"],
+    },
   },
   {
     timestamps: true,
