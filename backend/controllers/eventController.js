b468796539bd5e86af70229f014fb57ea0e52582
Anurag <anuragsharma011011@gmail.com>
2022-07-11 17:13:10 +0530

    bug-solvi'n time

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -9,7 +9,7 @@ const User = require('../models/userModel')
 
 const getEvents = asyncHandler( async (req, res) => {
 
-    const events = await Event.find({user:req.user.id})
+    const events = await Event.find()
     
     res.status(200).json(events)
 })
@@ -44,7 +44,6 @@ const setEvent = asyncHandler(async  (req,res)=>{
 
     const event = await Event.create({
         text : req.body.text,
-        user : req.user.id,
         location : req.body.location,
         imageurl : req.body.imageurl,
         website : req.body.website
@@ -66,17 +65,17 @@ const updateEvent = asyncHandler(async (req,res)=>{
         throw new Error('event Not Found')
     }
 
-    const user = await User.findById(req.user.id)
+    // const user = await User.findById(req.user.id)
 
-    if(!user){
-        res.status(401)
-        throw new Error('User not found')
-    }
+    // if(!user){
+    //     res.status(401)
+    //     throw new Error('User not found')
+    // }
 
-    if (event.user.toString() !== user.id){
-        res.status(401)
-        throw new Error('User not authorized')
-    }
+    // if (event.user.toString() !== user.id){
+    //     res.status(401)
+    //     throw new Error('User not authorized')
+    // }
 
     const updatedevent = await Event.findByIdAndUpdate(req.params.id, req.body,  {
         new:true,
@@ -97,17 +96,17 @@ const deleteEvent = asyncHandler(async (req,res) => {
         throw new Error('Event Not Found')
     }
 
-    const user = await User.findById(req.user.id)
+    // const user = await User.findById(req.user.id)
 
-    if(!user){
-        res.status(401)
-        throw new Error('User not found')
-    }
+    // if(!user){
+    //     res.status(401)
+    //     throw new Error('User not found')
+    // }
 
-    if (event.user.toString() !== user.id){
-        res.status(401)
-        throw new Error('User not authorized')
-    }
+    // if (event.user.toString() !== user.id){
+    //     res.status(401)
+    //     throw new Error('User not authorized')
+    // }
 
     await event.remove()
 
