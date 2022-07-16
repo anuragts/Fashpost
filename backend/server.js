45ef5a3f84e2b28a479fbf22f3753b624f42823b
Anurag <anuragsharma011011@gmail.com>
2022-07-17 02:11:35 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 const app = express()
+const path = require('path');
+
 
 connectDB()
 
@@ -13,6 +15,20 @@ app.use(express.urlencoded({extended:false}))
 
 app.use('/api/events', require('./routes/eventRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+
 
 app.use(errorHandler)
 app.use('/error', require('./routes/error'))
